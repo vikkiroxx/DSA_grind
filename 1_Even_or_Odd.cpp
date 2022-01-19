4f5fb10d91f35ae752bf94025440122ef7165a2a
Vikram Singh Chauhan <59952002+vikkiroxx@users.noreply.github.com>
2022-01-19 12:59:37 +0530

    Add files via upload

---
+++ 1_Even_or_Odd.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter Number:";
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        printf("Number is even");
+    }
+    else
+    {
+        printf("Number is odd");
+    }
+}
