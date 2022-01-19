33e64a752067413d9809cfe52e9cc8264c837df8
Vikram Singh Chauhan <59952002+vikkiroxx@users.noreply.github.com>
2022-01-19 13:07:00 +0530

    Add files via upload

---
+++ 10_LCM_of_2_no.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int GCD(int, int);
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers:";
+    cin >> a >> b;
+
+    cout << (a * b) / GCD(a, b);
+    return 0;
+}
+
+int GCD(int a, int b)
+{
+    int x, y, r;
+
+    if (a > b)
+    {
+        x = a;
+        y = b;
+    }
+    else
+    {
+        x = b;
+        y = a;
+    }
+    while (y != 0)
+    {
+        r = x % y;
+        x = y;
+        y = r;
+    }
+    return x;
+}
