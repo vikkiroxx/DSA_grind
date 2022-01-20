e842f4c146c57e0164516f0300fa5e8ee1e5057a
Vikram Singh Chauhan <59952002+vikkiroxx@users.noreply.github.com>
2022-01-20 12:06:38 +0530

    Add files via upload

---
+++ 16_length_of_string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char test[100];
+
+    cout << "Enter text:\n";
+    gets(test);
+
+    cout << "String Lenth: ";
+    cout << strlen(test);
+}
