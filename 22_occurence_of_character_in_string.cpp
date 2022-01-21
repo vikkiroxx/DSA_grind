6ad390ad5a2dd8c10ebe3a811fb8324e796fd9d8
Vikram Singh Chauhan <59952002+vikkiroxx@users.noreply.github.com>
2022-01-21 19:26:45 +0530

    Add files via upload

---
+++ 22_occurence_of_character_in_string.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "vitkimaakabharosa";
+    char c = 'a';
+
+    cout << count(s.begin(), s.end(), c);
+    return 0;
+}
