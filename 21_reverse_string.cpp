843ef47f63edde6016e9fe0bbb08e82a2bb820a3
Vikram Singh Chauhan <59952002+vikkiroxx@users.noreply.github.com>
2022-01-21 19:26:34 +0530

    Add files via upload

---
+++ 21_reverse_string.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string s = "vikram";
+
+    reverse(s.begin(), s.end());
+    cout << s;
+    return 0;
+}
