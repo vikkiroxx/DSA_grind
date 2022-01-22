5ec51754c1ba2e3594da66125a02e2f0d3f1e051
Vikram Singh Chauhan <59952002+vikkiroxx@users.noreply.github.com>
2022-01-22 10:21:32 +0530

    Add files via upload

---
+++ 38_add_two_matrices.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+    cout << "Enter number of rows: ";
+    cin >> r;
+    cout << "Enter number of columns: ";
+    cin >> c;
+
+    cout << "Enter elements of first matrix:" << endl;
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    cout << "Enter elements of second matrix:" << endl;
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            cin >> b[i][j];
+        }
+    }
+
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    cout << "Sum of matrices is: " << endl;
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            cout << sum[i][j] << " ";
+            if (j == c - 1)
+            {
+                cout << endl;
+            }
+        }
+    }
+    return 0;
+}
