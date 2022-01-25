3fac9f5bc660693319be36fc27e2bd5824ef373e
Vikram Singh Chauhan <59952002+vikkiroxx@users.noreply.github.com>
2022-01-25 20:33:37 +0530

    Add files via upload

---
+++ 54_square_root_of-a_no.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float n, root;
+    cout << "Enter number for rooting: ";
+    cin >> n;
+
+    root = sqrt(n);
+    cout << root;
+    return 0;
+}
